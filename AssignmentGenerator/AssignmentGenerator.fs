000df46b554dc9f69add2e5f9f738bedbfb3ea67
sideproject <sideproject@gmail.com>
2011-10-17 20:20:31 -0400

    one-time-run hard-coding james->elise

--- AssignmentGenerator/AssignmentGenerator.fs
+++ AssignmentGenerator/AssignmentGenerator.fs
@@ -15,44 +15,49 @@ type Assignment =
         To : Person
     }
 
-let eliz =          { FirstName="eliz"; LastName="1"; AgeType="child"; Previous=["martha";]}
-let margaret =      { FirstName="margaret"; LastName="1"; AgeType="parent"; Previous=["mary"; "anne"; "laura";]}
-let elise =         { FirstName="elise"; LastName="1"; AgeType="parent"; Previous=["james"; "hollie"; "sam";]}
-let christopher =   { FirstName="christopher"; LastName="1"; AgeType="child"; Previous=["shawn"; "daniel"; "anne";]}
-let julia =         { FirstName="julia"; LastName="1"; AgeType="child"; Previous=["carla"; "mallory"; "charlie"]}
-let charlie =       { FirstName="charlie"; LastName="2"; AgeType="parent"; Previous=["jimmy"; "dean"; "hollie";]}
-let anne =          { FirstName="anne"; LastName="2"; AgeType="parent"; Previous=["sarah"; "carla"; "christopher";]}
-let emily =         { FirstName="emily"; LastName="2"; AgeType="child"; Previous=["sam"; "james"; "andrew";]}
-let laura =         { FirstName="laura"; LastName="2"; AgeType="child"; Previous=["martha"; "andrew"; "jimmy"; ]}
-let jimmy =         { FirstName="jimmy"; LastName="3"; AgeType="parent"; Previous=["daniel"; "charlie"; "elise";]}
-let hollie =        { FirstName="hollie"; LastName="3"; AgeType="parent"; Previous=["elise"; "sarah"; "mallory";]}
-let andrew =        { FirstName="andrew"; LastName="3"; AgeType="child"; Previous=["joeseph"; "sam"; "frank";]}
-let amie =          { FirstName="amie"; LastName="3"; AgeType="child"; Previous=["emiliy"; "martha"; "emily";]}
-let frank =         { FirstName="frank"; LastName="4"; AgeType="parent"; Previous=["margaret"; "shawn"; "daniel";]}
-let carla =         { FirstName="carla"; LastName="4"; AgeType="parent"; Previous=["anne"; "mary"; "dean";]}
-let mallory =       { FirstName="mallory"; LastName="4"; AgeType="child"; Previous=["christopher"; "joseph"; "amie";]}
-let martha =        { FirstName="martha"; LastName="4"; AgeType="child"; Previous=["amie"; "julia"; "mary";]}
-let mary =          { FirstName="mary"; LastName="5"; AgeType="parent"; Previous=["hollie"; "elise"; "eliz";]}
-let dean =          { FirstName="dean"; LastName="5"; AgeType="parent"; Previous=["frank"; "jimmy"; "shawn";]}
-let james =         { FirstName="james"; LastName="5"; AgeType="child"; Previous=["mallory"; "christopher"; "sarah";]}
-let joeseph =       { FirstName="joeseph"; LastName="5"; AgeType="child"; Previous=["andrew"; "laura"; "julia";]}
-let sarah =         { FirstName="sarah"; LastName="6"; AgeType="parent"; Previous=["charlie"; "margaret"; "james";]}
-let shawn =         { FirstName="shawn"; LastName="6"; AgeType="parent"; Previous=["dean"; "frank"; "carla";]}
-let sam =           { FirstName="sam"; LastName="6"; AgeType="child"; Previous=["laura"; "amie"; "joseph";]}
-let daniel =        { FirstName="daniel"; LastName="6"; AgeType="child"; Previous=["julia"; "emily"; "margaret"; ]}
-
-let people = [eliz; margaret; elise; christopher; julia; 
-            charlie; anne; emily; laura; 
-            jimmy; hollie; andrew; amie; 
-            frank; carla; mallory; martha; 
-            mary; dean; james; joeseph; 
-            sarah; shawn; sam; daniel]
+//let eliz =          { FirstName="eliz"; LastName="1"; AgeType="child"; Previous=["martha";]}
+//let margaret =      { FirstName="margaret"; LastName="1"; AgeType="parent"; Previous=["mary"; "anne"; "laura";]}
+//let elise =         { FirstName="elise"; LastName="1"; AgeType="parent"; Previous=["james"; "hollie"; "sam";]}
+//let christopher =   { FirstName="christopher"; LastName="1"; AgeType="child"; Previous=["shawn"; "daniel"; "anne";]}
+//let julia =         { FirstName="julia"; LastName="1"; AgeType="child"; Previous=["carla"; "mallory"; "charlie"]}
+//let charlie =       { FirstName="charlie"; LastName="2"; AgeType="parent"; Previous=["jimmy"; "dean"; "hollie";]}
+//let anne =          { FirstName="anne"; LastName="2"; AgeType="parent"; Previous=["sarah"; "carla"; "christopher";]}
+//let emily =         { FirstName="emily"; LastName="2"; AgeType="child"; Previous=["sam"; "james"; "andrew";]}
+//let laura =         { FirstName="laura"; LastName="2"; AgeType="child"; Previous=["martha"; "andrew"; "jimmy"; ]}
+//let jimmy =         { FirstName="jimmy"; LastName="3"; AgeType="parent"; Previous=["daniel"; "charlie"; "elise";]}
+//let hollie =        { FirstName="hollie"; LastName="3"; AgeType="parent"; Previous=["elise"; "sarah"; "mallory";]}
+//let andrew =        { FirstName="andrew"; LastName="3"; AgeType="child"; Previous=["joeseph"; "sam"; "frank";]}
+//let amie =          { FirstName="amie"; LastName="3"; AgeType="child"; Previous=["emiliy"; "martha"; "emily";]}
+//let frank =         { FirstName="frank"; LastName="4"; AgeType="parent"; Previous=["margaret"; "shawn"; "daniel";]}
+//let carla =         { FirstName="carla"; LastName="4"; AgeType="parent"; Previous=["anne"; "mary"; "dean";]}
+//let mallory =       { FirstName="mallory"; LastName="4"; AgeType="child"; Previous=["christopher"; "joseph"; "amie";]}
+//let martha =        { FirstName="martha"; LastName="4"; AgeType="child"; Previous=["amie"; "julia"; "mary";]}
+//let mary =          { FirstName="mary"; LastName="5"; AgeType="parent"; Previous=["hollie"; "elise"; "eliz";]}
+//let dean =          { FirstName="dean"; LastName="5"; AgeType="parent"; Previous=["frank"; "jimmy"; "shawn";]}
+//let james =         { FirstName="james"; LastName="5"; AgeType="child"; Previous=["mallory"; "christopher"; "sarah";]}
+//let joeseph =       { FirstName="joeseph"; LastName="5"; AgeType="child"; Previous=["andrew"; "laura"; "julia";]}
+//let sarah =         { FirstName="sarah"; LastName="6"; AgeType="parent"; Previous=["charlie"; "margaret"; "james";]}
+//let shawn =         { FirstName="shawn"; LastName="6"; AgeType="parent"; Previous=["dean"; "frank"; "carla";]}
+//let sam =           { FirstName="sam"; LastName="6"; AgeType="child"; Previous=["laura"; "amie"; "joseph";]}
+//let daniel =        { FirstName="daniel"; LastName="6"; AgeType="child"; Previous=["julia"; "emily"; "margaret"; ]}
+//
+//let people = [eliz; margaret; elise; christopher; julia; 
+//            charlie; anne; emily; laura; 
+//            jimmy; hollie; andrew; amie; 
+//            frank; carla; mallory; martha; 
+//            mary; dean; james; joeseph; 
+//            sarah; shawn; sam; daniel]
 
 
 /////////////////////////////////////////////rules///////////////////////////////////////////////////////////////////
 let rule_not_self assignments = 
     let q = List.tryFind (fun x -> x.From.FirstName = x.To.FirstName) assignments
     q = None
+
+
+//let rule_temp_james_elise assignments = 
+//    let james = List.tryFind (fun x -> x.From.FirstName = "james") assignments
+//    james.Value.To.FirstName = "elise"
 
 let rule_not_same_family assignments =
     let q = List.tryFind (fun x -> x.From.LastName = x.To.LastName) assignments
@@ -174,6 +179,7 @@ let assignments_are_valid assignments =
     let distinct_families = Set.toList (set families)
 
     (rule_not_self assignments) 
+        //&& (rule_temp_james_elise assignments)
         && (rule_not_same_family assignments) 
         && (rule_no_previous_people assignments) 
         && (rule_max_one_difficult assignments) 
--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -1,31 +1,32 @@
 ﻿// Learn more about F# at http://fsharp.net
 open AssignmentGenerator
 
-let eliz =          { FirstName="eliz";         LastName="1"; AgeType="child";  Previous=[                              "martha";      "joeseph";]}
-let margaret =      { FirstName="margaret";     LastName="1"; AgeType="parent"; Previous=["mary";        "anne";        "laura";       "carla";]}
-let elise =         { FirstName="elise";        LastName="1"; AgeType="parent"; Previous=["james";       "hollie";      "sam";         "mallory";]}
-let christopher =   { FirstName="christopher";  LastName="1"; AgeType="child";  Previous=["shawn";       "daniel";      "anne";        "mary";]}
-let julia =         { FirstName="julia";        LastName="1"; AgeType="child";  Previous=["carla";       "mallory";     "charlie";     "shawn";]}
-let charlie =       { FirstName="charlie";      LastName="2"; AgeType="parent"; Previous=["jimmy";       "dean";        "hollie";      "elise";]}
-let anne =          { FirstName="anne";         LastName="2"; AgeType="parent"; Previous=["sarah";       "carla";       "christopher"; "james";]}
-let emily =         { FirstName="emily";        LastName="2"; AgeType="child";  Previous=["sam";         "james";       "andrew";      "frank";]}
-let laura =         { FirstName="laura";        LastName="2"; AgeType="child";  Previous=["martha";      "andrew";      "jimmy";       "sam";]}
-let jimmy =         { FirstName="jimmy";        LastName="3"; AgeType="parent"; Previous=["daniel";      "charlie";     "elise";       "martha";]}
-let hollie =        { FirstName="hollie";       LastName="3"; AgeType="parent"; Previous=["elise";       "sarah";       "mallory";     "charlie";]}
-let andrew =        { FirstName="andrew";       LastName="3"; AgeType="child";  Previous=["joeseph";     "sam";         "frank";       "christopher";]}
-let amie =          { FirstName="amie";         LastName="3"; AgeType="child";  Previous=["emiliy";      "martha";      "emily";       "daniel";]}
-let frank =         { FirstName="frank";        LastName="4"; AgeType="parent"; Previous=["margaret";    "shawn";       "daniel";      "sarah";]}
-let carla =         { FirstName="carla";        LastName="4"; AgeType="parent"; Previous=["anne";        "mary";        "dean";        "eliz";]}
-let mallory =       { FirstName="mallory";      LastName="4"; AgeType="child";  Previous=["christopher"; "joseph";      "amie";        "emily";]}
-let martha =        { FirstName="martha";       LastName="4"; AgeType="child";  Previous=["amie";        "julia";       "mary";        "anne";]}
-let mary =          { FirstName="mary";         LastName="5"; AgeType="parent"; Previous=["hollie";      "elise";       "eliz";        "margaret";]}
-let dean =          { FirstName="dean";         LastName="5"; AgeType="parent"; Previous=["frank";       "jimmy";       "shawn";       "andrew";]}
-let james =         { FirstName="james";        LastName="5"; AgeType="child";  Previous=["mallory";     "christopher"; "sarah";       "laura";]}
-let joeseph =       { FirstName="joeseph";      LastName="5"; AgeType="child";  Previous=["andrew";      "laura";       "julia";       "hollie";]}
-let sarah =         { FirstName="sarah";        LastName="6"; AgeType="parent"; Previous=["charlie";     "margaret";    "james";       "julia";]}
-let shawn =         { FirstName="shawn";        LastName="6"; AgeType="parent"; Previous=["dean";        "frank";       "carla";       "amie";]}
-let sam =           { FirstName="sam";          LastName="6"; AgeType="child";  Previous=["laura";       "amie";        "joseph";      "jimmy";]}
-let daniel =        { FirstName="daniel";       LastName="6"; AgeType="child";  Previous=["julia";       "emily";       "margaret";    "dean";]}
+let eliz =          { FirstName="eliz";         LastName="1"; AgeType="child";  Previous=[                              "martha";      "sam" ]}
+let margaret =      { FirstName="margaret";     LastName="1"; AgeType="parent"; Previous=["mary";        "anne";        "laura";       "daniel" ]}
+let elise =         { FirstName="elise";        LastName="1"; AgeType="parent"; Previous=["james";       "hollie";      "sam";         "anne" ]}
+let christopher =   { FirstName="christopher";  LastName="1"; AgeType="child";  Previous=["shawn";       "daniel";      "anne";        "carla" ]}
+let julia =         { FirstName="julia";        LastName="1"; AgeType="child";  Previous=["carla";       "mallory";     "charlie";     "dean" ]}
+let charlie =       { FirstName="charlie";      LastName="2"; AgeType="parent"; Previous=["jimmy";       "dean";        "hollie";      "margaret" ]}
+let anne =          { FirstName="anne";         LastName="2"; AgeType="parent"; Previous=["sarah";       "carla";       "christopher"; "frank" ]}
+let emily =         { FirstName="emily";        LastName="2"; AgeType="child";  Previous=["sam";         "james";       "andrew";      "christopher" ]}
+let laura =         { FirstName="laura";        LastName="2"; AgeType="child";  Previous=["martha";      "andrew";      "jimmy";       "joeseph" ]}
+let jimmy =         { FirstName="jimmy";        LastName="3"; AgeType="parent"; Previous=["daniel";      "charlie";     "elise";       "mary" ]}
+let hollie =        { FirstName="hollie";       LastName="3"; AgeType="parent"; Previous=["elise";       "sarah";       "mallory";     "shawn" ]}
+let andrew =        { FirstName="andrew";       LastName="3"; AgeType="child";  Previous=["joeseph";     "sam";         "frank";       "laura" ]}
+let amie =          { FirstName="amie";         LastName="3"; AgeType="child";  Previous=["emiliy";      "martha";      "emily";       "mallory" ]}
+let frank =         { FirstName="frank";        LastName="4"; AgeType="parent"; Previous=["margaret";    "shawn";       "daniel";      "emily" ]}
+let carla =         { FirstName="carla";        LastName="4"; AgeType="parent"; Previous=["anne";        "mary";        "dean";        "hollie" ]}
+let mallory =       { FirstName="mallory";      LastName="4"; AgeType="child";  Previous=["christopher"; "joseph";      "amie";        "james" ]}
+let martha =        { FirstName="martha";       LastName="4"; AgeType="child";  Previous=["amie";        "julia";       "mary";        "eliz" ]}
+let mary =          { FirstName="mary";         LastName="5"; AgeType="parent"; Previous=["hollie";      "elise";       "eliz";        "andrew" ]}
+let dean =          { FirstName="dean";         LastName="5"; AgeType="parent"; Previous=["frank";       "jimmy";       "shawn";       "martha" ]}
+let james =         { FirstName="james";        LastName="5"; AgeType="child";  Previous=["mallory";     "christopher"; "sarah";       "elise" ]}
+let joeseph =       { FirstName="joeseph";      LastName="5"; AgeType="child";  Previous=["andrew";      "laura";       "julia";       "sarah" ]}
+let sarah =         { FirstName="sarah";        LastName="6"; AgeType="parent"; Previous=["charlie";     "margaret";    "james";       "julia" ]}
+let shawn =         { FirstName="shawn";        LastName="6"; AgeType="parent"; Previous=["dean";        "frank";       "carla";       "jimmy" ]}
+let sam =           { FirstName="sam";          LastName="6"; AgeType="child";  Previous=["laura";       "amie";        "joseph";      "charlie" ]}
+let daniel =        { FirstName="daniel";       LastName="6"; AgeType="child";  Previous=["julia";       "emily";       "margaret";    "amie" ]}
+  
 
 let people = [  eliz; margaret; elise; christopher; julia; 
                 charlie; anne; emily; laura; 
